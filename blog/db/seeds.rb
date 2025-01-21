7e35752a081b679cbc3f1b38e442cb2cb27aafec
Juan Medina <juan.ms@outlook.com>
2025-01-20 21:00:20 -0600

    Add MVC tutorial

---
+++ blog/db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+Post.create(title: 'Hello World', content: 'This is my first post')
+Post.create(title: 'Second Post', content: 'This is my second post')
+
+
+puts "Done seeding!"
